fd009a7c1e1b0ae09d7d56882f5ee920436f11a9
alynn0977 <82777340+alynn0977@users.noreply.github.com>
2023-09-06 22:21:37 -0400

    SUS-50-BallImproved

--- SushiTime/Assets/SystemAssets/BreakoutSystem/Runtime/BallBehaviour.cs
+++ SushiTime/Assets/SystemAssets/BreakoutSystem/Runtime/BallBehaviour.cs
@@ -10,37 +10,27 @@ namespace BreakoutSystem
     /// </summary>
     public class BallBehaviour : MonoBehaviour
     {
-
-        private const int boundaryLimit = 3;
-
         [SerializeField]
         private float speed = 20f;
-
         [SerializeField]
-        private float maxBounceAngle = 75f;
+        private float initialSpeed = 2f;
+        
+        private Vector3 initialDirection;
 
         private GameZone gameZone;
-      
-        [field: SerializeField]
-        public float Speed { get; set; }
         
         [field: SerializeField]
-        public Vector2 Vector { get; set; }
+        public Vector2 Velocity { get; set; }
 
         [field: SerializeField]
         public int CurrentPlayerPwr { get; private set; }
 
-        [field: SerializeField]
-        public int CurrentBoundaryHits { get; private set; }
-
         public Rigidbody2D ThisRigidBody { get; private set; }
-        
-
 
         private void Awake()
         {
             ThisRigidBody = GetComponent<Rigidbody2D>();
-
+            ThisRigidBody.velocity = ReturnRandomTraj().normalized * initialSpeed;
             if (GetComponentInParent<GameZone>())
             {
                 gameZone = GetComponentInParent<GameZone>();
@@ -56,34 +46,14 @@ namespace BreakoutSystem
                 Debug.LogWarning("[BallBehaviour]: No gamezone found. Initializing with default.");
                 CurrentPlayerPwr = 1;
             }
-
-        }
-
-        private void Start()
-        {
-            Invoke(nameof(ReturnRandomTraj), .4f);
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (collision.gameObject.TryGetComponent(out BallBehaviour ballBehaviour))
-            {
-                BallCollision(collision);
-            }
-            else if (collision.gameObject.TryGetComponent(out iInteractable interactable))
+            // Ball activates certain game objects.
+            if (collision.gameObject.TryGetComponent(out iInteractable interactable))
             {
                 Interact(collision);
-            }
-            else if (collision.gameObject.tag == "Boundary")
-            {
-                if (CurrentBoundaryHits >= boundaryLimit)
-                {
-                    ReturnRandomTraj();
-                    CurrentBoundaryHits = 0;
-                    return;
-                }
-
-                CurrentBoundaryHits++;
             }
         }
 
@@ -95,48 +65,26 @@ namespace BreakoutSystem
             }
         }
 
-        private void BallCollision(Collision2D collision)
+        private Vector3 ReturnRandomTraj()
         {
-            if (collision.gameObject.TryGetComponent(out BallBehaviour ball))
-            {
-                float calculateNewAngle = CalculateRotation(ball, collision);
-                Quaternion rotation = Quaternion.AngleAxis(calculateNewAngle, Vector3.forward);
-                ball.ThisRigidBody.velocity = rotation * Vector2.up * ball.ThisRigidBody.velocity.magnitude;
-            }
-        }
-
-        private float CalculateRotation(BallBehaviour ball, Collision2D collision)
-        {
-            Vector3 paddlePosition = transform.position;
-            Vector2 contactPoint = collision.GetContact(0).point;
-
-            float offset = paddlePosition.x - contactPoint.x;
-            float width = collision.otherCollider.bounds.size.x * 5f;
-
-            float currentAngle = Vector2.SignedAngle(Vector2.up, ball.ThisRigidBody.velocity);
-            float bounceAngle = (offset / width) * maxBounceAngle;
-
-            float newAngle = Mathf.Clamp(currentAngle + bounceAngle, -maxBounceAngle, maxBounceAngle);
-
-            return newAngle;
-        }
-        private void ReturnRandomTraj()
-        {
-            Vector2 force = Vector2.zero;
+            Vector3 force = Vector2.zero;
             force.x = Random.Range(-1f, 1f);
             force.y = -1f;
 
-            ThisRigidBody.AddForce(force.normalized * speed);
-        }
-
-        private void Update()
-        {
-            Speed = ThisRigidBody.velocity.magnitude;
+            return force;
         }
 
         private void FixedUpdate()
         {
-            ThisRigidBody.velocity = ThisRigidBody.velocity.normalized* speed;
+            if (Mathf.Abs(ThisRigidBody.velocity.y) < 0.5f)
+            {
+                Vector3 newVelocity = ThisRigidBody.velocity;
+                newVelocity.y = newVelocity.y < 0 ? -1f : 1f;
+                ThisRigidBody.velocity = newVelocity;
+            }
+
+            // Normalize the velocity to maintain a constant speed
+            ThisRigidBody.velocity = ThisRigidBody.velocity.normalized * speed;
         }
 
         private void PlayerPowerIncrease(IncreasePowerEvent e)
